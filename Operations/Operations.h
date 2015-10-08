5afc21f5d1c01858a55bf0603222213f90b3d18b
zen <zen.dev@yandex.com>
2015-10-08 18:27:16 +0300

    Init

---
+++ Operations/Operations.h
@@ -0,0 +1,19 @@
+//
+//  Operations.h
+//  Operations
+//
+//  Created by zen on 08/10/15.
+//  Copyright © 2015 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Operations.
+FOUNDATION_EXPORT double OperationsVersionNumber;
+
+//! Project version string for Operations.
+FOUNDATION_EXPORT const unsigned char OperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Operations/PublicHeader.h>
+
+
